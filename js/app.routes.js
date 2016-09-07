21eb06080593d65f587424fd9d5f3f555c395a24
Daniel Landau <danieltotherescue@gmail.com>
2016-09-07 11:07:32 -0700

    updated routes to make the deloyed page work

--- js/app.routes.js
+++ js/app.routes.js
@@ -11,43 +11,43 @@
     $stateProvider
       .state("welcome", {
         url:         "/",
-        templateUrl: "/js/templates/welcome.html"
+        templateUrl: "./js/templates/welcome.html"
       })
       .state("signin", {
         url:          "/signin",
-        templateUrl:  "/js/templates/signin.html",
+        templateUrl:  "./js/templates/signin.html",
         controller:   "SignInController",
         controllerAs: "vm"
       })
       .state("signup", {
         url:          "/signup",
-        templateUrl:  "/js/templates/signup.html",
+        templateUrl:  "./js/templates/signup.html",
         controller:   "SignInController",
         controllerAs: "vm"
       })
       .state("profile", {
         url:         "/profile",
-        templateUrl: "/js/templates/profile.html"
+        templateUrl: "./js/templates/profile.html"
       })
       .state("about", {
         url:         "/about",
-        templateUrl: "/js/templates/about.html"
+        templateUrl: "./js/templates/about.html"
       })
       .state("compose", {
         url:         "/compose",
-        templateUrl: "/js/templates/compose.html"
+        templateUrl: "./js/templates/compose.html"
       })
       .state("completed-msgs", {
         url:         "/completed-msgs",
-        templateUrl: "/js/templates/completed-msgs.html"
+        templateUrl: "./js/templates/completed-msgs.html"
       })
       .state("realize-msg", {
         url:         "/realize-msg",
-        templateUrl: "/js/templates/realize-msg.html"
+        templateUrl: "./js/templates/realize-msg.html"
       })
       .state("select-msg", {
         url:         "/select-msg",
-        templateUrl: "/js/templates/select-msg.html"
+        templateUrl: "./js/templates/select-msg.html"
       });
 
     $urlRouterProvider.otherwise("/");
